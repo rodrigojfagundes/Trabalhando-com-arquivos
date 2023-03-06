f73fbefec37d11ef95eed62520b3013497705b93
Rodrigo Jose Fagundes <rodrigojosefagundes@gmail.com>
2023-03-06 17:35:19 -0300

    Manipulando pastas com File
    Aula 219.

---
+++ 219 - Manipulando pastas com File/course/src/course/Program.java
@@ -0,0 +1,36 @@
+package course;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("enter a folder path: ");
+		
+		String strPath = sc.nextLine();
+		
+		File path = new File(strPath);
+		
+		File[] folders = path.listFiles(File::isDirectory);
+		System.out.println("FOLDERS");
+		
+		for (File folder : folders) {
+		
+			System.out.println(folder);
+		}
+		
+		File[] files = path.listFiles(File::isFile);
+		System.out.println("FILES");
+		for (File file : files) {
+			System.out.println(file);
+		}
+		
+		boolean success = new File(strPath + "\\pastaTeste").mkdir();
+		System.out.println("Directory created successfully: " + success);
+		sc.close();
+	}
+}
